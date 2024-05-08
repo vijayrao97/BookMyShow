a0b256a240850f201d3546392e281c64ee0b0140
vijayrao97 <vijayrao3113@gmail.com>
2024-05-08 09:29:44 +0530

    Repo layer added and code added for controller and Service layer
    Signed-off-by: vijayrao97 <vijayrao3113@gmail.com>

---
+++ src/main/java/Repo/ShowRepository.java
@@ -0,0 +1,11 @@
+package Repo;
+
+import models.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ShowRepository extends JpaRepository<Show,Long> {
+    @Override
+    Optional<Show> findById(Long aLong);
+}
--- src/main/java/Service/BookingService.java
+++ src/main/java/Service/BookingService.java
@@ -1,4 +1,57 @@
 package Service;
 
+
+import Repo.ShowRepository;
+import Repo.UserRepository;
+import models.Booking;
+import models.Show;
+import models.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class BookingService {
+    private ShowRepository showRepo;
+    private UserRepository userRepo;
+
+    public BookingService(ShowRepository showRepository, UserRepository userRepository){
+        this.showRepo = showRepository;
+        this.userRepo = userRepository;
+    }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public Booking CreateBooking(Long userId, List<Long> showSeat, Long showId){
+                /*
+
+        1. check user if user exists..
+        2. check if show exist..
+        3. get all showSeat Ids objects and see if they exist...
+        4. ----TAKE LOCK---
+        5. check if seat available or not..
+        6. if not throw error..
+        7. if yes, Mark the status of all seats to blocked and save to db..
+        8. --- RELEASE LOCK ---
+        9. create a booking object with pending status..
+        10. returning booking object..
+
+         */
+        Optional<User> userOptional = this.userRepo.findById(userId);
+        if( userOptional.isEmpty() ){
+            throw new RuntimeException("User is not present.");
+        }
+        User u = userOptional.get();
+        Optional<Show> showOptional = this.showRepo.findById(userId);
+        if( showOptional.isEmpty() ){
+            throw new RuntimeException("Show not available");
+        }
+
+        // TODO: complete step 3, 5, 6,7(Except save), 9
+
+        return null;
+    }
+
 }
