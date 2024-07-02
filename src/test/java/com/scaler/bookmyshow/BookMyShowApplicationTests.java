d09e0c91ec27650cfeb51ec03368dc563ecd1fd7
vijayrao97 <vijayrao3113@gmail.com>
2024-07-02 10:30:12 +0530

    Test cases added for signup
    Signed-off-by: vijayrao97 <vijayrao3113@gmail.com>

--- src/test/java/com/scaler/bookmyshow/BookMyShowApplicationTests.java
+++ src/test/java/com/scaler/bookmyshow/BookMyShowApplicationTests.java
@@ -2,6 +2,10 @@ package com.scaler.bookmyshow;
 
 import com.scaler.bookmyshow.Repo.BookingRepo;
 import com.scaler.bookmyshow.Repo.UserRepository;
+import com.scaler.bookmyshow.Service.UserService;
+import com.scaler.bookmyshow.controller.UserController;
+import com.scaler.bookmyshow.dto.SignupRequestDto;
+import com.scaler.bookmyshow.dto.SignupResponseDto;
 import com.scaler.bookmyshow.models.Booking;
 import com.scaler.bookmyshow.models.User;
 import org.junit.jupiter.api.Test;
@@ -12,8 +16,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BookMyShowApplicationTests {
 
+    @Autowired
+    private UserController userController;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testSignUpFunctionality(){
+        SignupRequestDto signupRequestDto = new SignupRequestDto();
+        signupRequestDto.setName("Vijay");
+        signupRequestDto.setEmail("Vijay@gmail.com");
+        signupRequestDto.setPassword("rock@13");
+
+        SignupResponseDto signupResponseDto = userController.signUp(signupRequestDto);
+        System.out.println("User id is "+ signupResponseDto.getUserId());
+
     }
 
 
